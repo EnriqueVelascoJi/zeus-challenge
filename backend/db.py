cb363bb2a46c3abc2c036e264928e4ffd85181bb
Enrique Velasco Jimenez <enriquevelascojimenez@192.168.1.75>
2025-02-12 03:04:35 -0600

    Feature: change DB params

--- backend/db.py
+++ backend/db.py
@@ -8,7 +8,7 @@ POSTGRES_USER=os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD")
 POSTGRES_PORT=os.getenv("POSTGRES_PORT")
 
-url_connection = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:{POSTGRES_PORT}/{POSTGRES_DB}'
+url_connection = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db.vgmnnonyynccbvfwjrgf.supabase.co:{POSTGRES_PORT}/{POSTGRES_DB}'
 engine = create_engine(url_connection)
 try:
     with engine.connect() as connection_str:
